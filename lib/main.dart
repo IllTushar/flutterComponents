99b33512e70beb883797b12c75eb3d8b9efc8b9d
illTushar <gtushar7668@gmail.com>
2024-06-12 23:03:33 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter App!!" ,style: TextStyle(fontWeight: FontWeight.w100,color: Colors.deepOrange),),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Container(
+            child: Center(child: const Text("Container Box")),
+            color: Colors.amber,
+            width: 150,
+            height: 150,
+          ),
+        ),
+      ),
+    );
+  }
+}
