83aea8bf9b079e94ecf00b0dd1a066fe6c5a7314
illTushar <gtushar7668@gmail.com>
2024-06-17 23:47:27 +0530

    date time

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:ffi';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 void main() {
   runApp(MaterialApp(
     home: MyApp(),
@@ -17,8 +17,7 @@ class MyApp extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.green, // Set your desired color here
     ));
-    var email = TextEditingController();
-    var password = TextEditingController();
+    var time = DateTime.now();
     return SafeArea(
       child: Scaffold(
           appBar: AppBar(
@@ -33,67 +32,10 @@ class MyApp extends StatelessWidget {
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Center(
-                child: Container(
-                  width: 300,
-                  height: 50,
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: email,
-                    decoration: InputDecoration(
-                        hintText: "Enter Email",
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(11),
-                            borderSide:
-                                BorderSide(color: Colors.orange, width: 2)),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(11),
-                            borderSide:
-                                BorderSide(color: Colors.blue, width: 2)),
-                        prefixIcon: Icon(Icons.email_outlined),
-                        prefixIconColor: Colors.orangeAccent),
-                  ),
-                ),
-              ),
               Container(
                 width: double.infinity,
-                height: 20,
-              ),
-              Center(
-                child: Container(
-                  width: 300,
-                  height: 50,
-                  child: TextField(
-                    obscureText: true,
-                    controller: password,
-                    obscuringCharacter: "*",
-                    decoration: InputDecoration(
-                      hintText: "Enter Password",
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(11),
-                          borderSide:
-                              BorderSide(color: Colors.orange, width: 2)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(11),
-                          borderSide: BorderSide(color: Colors.blue, width: 2)),
-                      prefixIcon: Icon(Icons.key),
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.visibility),
-                        onPressed: () {
-                          Icon(Icons.visibility_off);
-                        },
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Center(
-                child: ElevatedButton(
-                  child: Text("Login",style: TextStyle(fontFamily: 'Font_Main'),),
-                  onPressed: (){
-                    print("Email ${email.text.toString().trim()} , password ${password.text.toString().trim()}");
-                  },
-                ),
+                height: 50,
+                child: Center(child: Text("Current time: ${DateFormat('yMMMMEEEEd').format(time)}")),
               )
             ],
           )),
