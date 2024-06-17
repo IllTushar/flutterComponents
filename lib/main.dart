ed15ec848cf903a1d4807f7d6922e72a6f3bae9e
illTushar <gtushar7668@gmail.com>
2024-06-17 14:58:25 +0530

    text_input

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class MyApp extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.green, // Set your desired color here
     ));
+    var email = TextEditingController();
+    var password = TextEditingController();
     return SafeArea(
       child: Scaffold(
           appBar: AppBar(
@@ -28,18 +30,72 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.blue,
           ),
           //ListView.builder is used to like recyclerview
-          body: Center(
-            child: Card(
-              shadowColor: Colors.orangeAccent,
-              elevation: 40,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  "Hello Tushar!!",
-                  style: TextStyle(fontFamily: 'Font_Main', fontSize: 40),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Container(
+                  width: 300,
+                  height: 50,
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: email,
+                    decoration: InputDecoration(
+                        hintText: "Enter Email",
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(11),
+                            borderSide:
+                                BorderSide(color: Colors.orange, width: 2)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(11),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2)),
+                        prefixIcon: Icon(Icons.email_outlined),
+                        prefixIconColor: Colors.orangeAccent),
+                  ),
                 ),
               ),
-            ),
+              Container(
+                width: double.infinity,
+                height: 20,
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  height: 50,
+                  child: TextField(
+                    obscureText: true,
+                    controller: password,
+                    obscuringCharacter: "*",
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(11),
+                          borderSide:
+                              BorderSide(color: Colors.orange, width: 2)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(11),
+                          borderSide: BorderSide(color: Colors.blue, width: 2)),
+                      prefixIcon: Icon(Icons.key),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.visibility),
+                        onPressed: () {
+                          Icon(Icons.visibility_off);
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                  child: Text("Login",style: TextStyle(fontFamily: 'Font_Main'),),
+                  onPressed: (){
+                    print("Email ${email.text.toString().trim()} , password ${password.text.toString().trim()}");
+                  },
+                ),
+              )
+            ],
           )),
     );
   }
